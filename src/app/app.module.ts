d3f5c03bc45bbf15dc7b52530172ba10900d6478
DeniseRisia <risiaresponde@gmail.com>
2025-05-14 19:34:57 -0300

    inicio do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+import { EsqueciSenhaComponent } from './esqueci-senha/esqueci-senha.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    LoginComponent,
+    EsqueciSenhaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sistema/sistema.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SistemaRoutingModule } from './sistema-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SistemaRoutingModule
+  ]
+})
+export class SistemaModule { }
