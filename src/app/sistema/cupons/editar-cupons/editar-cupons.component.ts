3a974b4b8a577235d486c054444303fc9676bedc
DiegoRibeiroo <diego09022004@gmail.com>
2025-06-06 18:02:24 -0300

    feat: criação da tela de cadastrar e editar cupom

---
+++ src/app/sistema/cupons/editar-cupons/editar-cupons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-cupons',
+  templateUrl: './editar-cupons.component.html',
+  styleUrls: ['./editar-cupons.component.css']
+})
+export class EditarCuponsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
