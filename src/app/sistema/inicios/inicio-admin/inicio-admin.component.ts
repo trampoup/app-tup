683dff1bcdcfc1f0d92326e5fa65deda6f6be1be
DiegoRibeiroo <diego09022004@gmail.com>
2025-08-18 19:19:50 -0300

    feat: paginação em destaques e serviços

--- src/app/sistema/inicios/inicio-admin/inicio-admin.component.ts
+++ src/app/sistema/inicios/inicio-admin/inicio-admin.component.ts
@@ -41,12 +41,71 @@ export class InicioAdminComponent implements OnInit {
     {
       titulo: 'destaque6',
       imagem: 'assets/imagens/imagens-de-exemplo/profissional-exemplo.png'
+    },
+    {
+      titulo: 'destaque7',
+      imagem: 'assets/imagens/imagens-de-exemplo/profissional-exemplo.png'
+    },
+    {
+      titulo: 'destaque8',
+      imagem: 'assets/imagens/imagens-de-exemplo/profissional-exemplo.png'
+    },
+    {
+      titulo: 'destaque9',
+      imagem: 'assets/imagens/imagens-de-exemplo/profissional-exemplo.png'
+    },
+    {
+      titulo: 'destaque10',
+      imagem: 'assets/imagens/imagens-de-exemplo/profissional-exemplo.png'
     }
   ];
+
+
+  paginaAtual: number = 1;
+  itensPorPagina: number = 8;
+  totalPaginas: number = Math.ceil(this.destaques.length / this.itensPorPagina);
+  destaquesPaginados : any[] = [];
+
+  paginaAtualServicos: number = 1;
+  itensPorPaginaServicos: number = 8;
+  totalPaginasServicos: number = Math.ceil(this.anuncios.length / this.itensPorPagina);
+  servicosPaginados : any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.atualizarPaginacaoDestaques();
+    this.atualizarPaginacaoServicos();
   }
 
+  atualizarPaginacaoDestaques(): void {
+    const inicio = (this.paginaAtual - 1) * this.itensPorPagina;
+    const fim = inicio + this.itensPorPagina;
+    this.destaquesPaginados = this.destaques.slice(inicio, fim);
+  }
+
+  get totalItens() {
+    return this.destaques.length;
+  }
+
+  onPaginaMudou(novaPagina: number) {
+    this.paginaAtual = novaPagina;
+    this.atualizarPaginacaoDestaques();
+  }
+
+
+  atualizarPaginacaoServicos(): void {
+    const inicio = (this.paginaAtualServicos - 1) * this.itensPorPaginaServicos;
+    const fim = inicio + this.itensPorPaginaServicos;
+    this.servicosPaginados = this.destaques.slice(inicio, fim); //SUBSTITUIR POR SERVIÇOS QUANDO IMPLEMENTAR
+  }
+
+  get totalItensServicos() {
+    return this.destaques.length; //SUBSTITUIR POR SERVIÇOS QUANDO IMPLEMENTAR
+  }
+
+  onPaginaMudouServicos(novaPagina: number) {
+    this.paginaAtualServicos = novaPagina;
+    this.atualizarPaginacaoServicos();
+  }
 }
